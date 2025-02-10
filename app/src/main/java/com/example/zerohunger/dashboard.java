788e2254781476b9e233fbef7f70981a44ac071e
punsarawikramarathna <punsarawikramarathna@gmail.com>
2025-02-10 22:16:42 +0530

    created dashboard interface

---
+++ app/src/main/java/com/example/zerohunger/dashboard.java
@@ -0,0 +1,98 @@
+package com.example.zerohunger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.security.identity.CredentialDataRequest;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Dashboard extends AppCompatActivity {
+
+    private CardView donatecard;
+    private CardView receiveCard;
+
+    private CardView historycard;
+
+    private CardView foodmapCard;
+
+    private CardView aboutusCard;
+
+    private CardView logoutCard;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dashboard);
+
+
+        donatecard= findViewById(R.id.donatecard);
+        receiveCard= findViewById(R.id.receiveCard);
+        historycard= findViewById(R.id.historycard);
+        foodmapCard= findViewById(R.id.foodmapCard);
+        aboutusCard= findViewById(R.id.aboutusCard);
+        logoutCard= findViewById(R.id.logoutCard);
+
+        donatecard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, DonateActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        receiveCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, ReceivedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        foodmapCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, FoodMapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        historycard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, HistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        aboutusCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, AboutusActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        logoutCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
