14f6b8e8b5c641e0062be5c11e014d17e3ae23a7
punsarawikramarathna <punsarawikramarathna@gmail.com>
2025-02-10 22:25:43 +0530

    created food map interface

---
+++ app/src/main/java/com/example/zerohunger/food_map.java
@@ -0,0 +1,12 @@
+package com.example.zerohunger;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class food_map extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_map);
+    }
+}
